a5e8629046b29606f30bca5a1e021d58910f3e57
Omar Khodr <omar.khoder2000@gmail.com>
2020-07-07 15:45:24 +0300

    Initial Commit

---
+++ Anghami-Playlists-Reimagined/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Anghami-Playlists-Reimagined
+//
+//  Created by Omar Khodr on 7/7/20.
+//  Copyright © 2020 Omar Khodr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
